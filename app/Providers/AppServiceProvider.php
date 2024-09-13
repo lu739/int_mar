61217a0d29ef51d4a6b28a0203099e00622b7b69
LidiaGodo <lilula739@gmail.com>
2024-09-14 00:06:16 +0300

    add install command

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Carbon\CarbonInterval;
+use Illuminate\Database\Connection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Http\Kernel;
 use Illuminate\Support\Facades\DB;
@@ -27,14 +28,18 @@ class AppServiceProvider extends ServiceProvider
         Model::shouldBeStrict(!app()->isProduction());
         Model::preventSilentlyDiscardingAttributes(!app()->isProduction());
 
-        DB::whenQueryingForLongerThan(500, function () {
-
+        DB::whenQueryingForLongerThan(500, function (Connection $connection) {
+            logger()
+                ->channel('telegram')
+                ->debug('Querying for longer than 500ms: ' . $connection->query()->toSql());
         });
 
         $kernel = app(Kernel::class);
         $kernel->whenRequestLifecycleIsLongerThan(
-            CarbonInterval::seconds(20), function () {
-
+            CarbonInterval::seconds(5), function () {
+                logger()
+                    ->channel('telegram')
+                    ->debug('Request lifecycle is longer than 5 seconds: ' . request()->url());
             }
         );
     }
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    logger()->channel('telegram')->debug('Hello');
-
     return view('welcome');
 });
