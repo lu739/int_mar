8a7031afcb79193ea6cca6ff06a59a890eee6d89
LidiaGodo <lilula739@gmail.com>
2024-09-16 21:08:41 +0300

    add db and seeders

--- app/Services/Telegram/TelegramBotApi.php
+++ app/Services/Telegram/TelegramBotApi.php
@@ -1,16 +1,25 @@
 <?php
 
 namespace App\Services\Telegram;
+use App\Services\Telegram\Exceptions\TelegramBotApiException;
 use Illuminate\Support\Facades\Http;
 
 final class TelegramBotApi
 {
     public const HOST = 'https://api.telegram.org/bot';
-    public static function sendMessage(int $chatId, string $token, string $message): void
+    public static function sendMessage(int $chatId, string $token, string $message): bool
     {
-        Http::get(self::HOST . $token . "/sendMessage", [
-            'chat_id' => $chatId,
-            'text' => $message,
-        ]);
+        try {
+            $response = Http::get(self::HOST . $token . "/sendMessage", [
+                'chat_id' => $chatId,
+                'text' => $message,
+            ])
+                ->throw()
+                ->json();
+
+            return $response['ok'] ?? false;
+        } catch (\Throwable $e) {
+            throw new TelegramBotApiException($e->getMessage());
+        }
     }
 }
