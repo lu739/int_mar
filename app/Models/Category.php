ffbb12da04b9e0298df92cb811ea19474117c05d
LidiaGodo <lilula739@gmail.com>
2024-09-17 11:35:11 +0300

    update db and seeders add console command

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,26 +2,20 @@
 
 namespace App\Models;
 
+use App\Traits\Model\HasSlug;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
     use HasFactory;
+    use HasSlug;
 
     protected $fillable = [
         'slug',
         'title',
     ];
 
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::creating(function (Category $category) {
-            $category->slug = $category->slug ?? str($category->title)->slug();
-        });
-    }
 
     public function products()
     {
